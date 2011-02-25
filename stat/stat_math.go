72df6fe052e2c0c84fa8fd09a51d8a018e3b2ee4
John Asmuth <jasmuth@gmail.com>
2011-02-25 17:21:41 -0500

    futile attempts to speed up Ln?

--- stat/stat_math.go
+++ stat/stat_math.go
@@ -62,14 +62,17 @@ var lanczos_coef []float64 = []float64{
 var Γ = math.Gamma
 
 var sqrt2pi = math.Sqrt(2 * math.Pi)
+var logsqrt2pi = log(math.Sqrt(2 * math.Pi))
 
-func LnΓ(x float64) float64 {
-	tmp := (x-0.5)*log(x+4.5) - (x + 4.5)
-	ser := 1.0 +
+func LnΓ(x float64) (res float64) {
+	res = (x - 0.5) * log(x+4.5) - (x + 4.5)
+	res += logsqrt2pi
+	res += log(1.0 +
 		76.1800917300/(x+0) - 86.5053203300/(x+1) +
 		24.0140982200/(x+2) - 1.23173951600/(x+3) +
-		0.00120858003/(x+4) - 0.00000536382/(x+5)
-	return tmp + log(ser*sqrt2pi)
+		0.00120858003/(x+4) - 0.00000536382/(x+5))
+
+	return
 }
 
 func B(x float64, y float64) float64 {
--- stat/stat_test.go
+++ stat/stat_test.go
@@ -1,16 +1,18 @@
 package stat
 
 import (
-	"testing";
-	"fmt";
+	"time"
+	"math"
+	"testing"
+	"fmt"
 )
 
-func TestDir(t *testing.T) {
-	α := []float64{4,5,6}
+func XTestDir(t *testing.T) {
+	α := []float64{4, 5, 6}
 	dgen := Dirichlet(α)
-	counts := [3]int{0,0,0}
+	counts := [3]int{0, 0, 0}
 	const total = 150000
-	for i:=0;i<total;i++ {
+	for i := 0; i < total; i++ {
 		θ := dgen()
 		v := NextChoice(θ)
 		counts[v]++
@@ -18,26 +20,60 @@ func TestDir(t *testing.T) {
 	fmt.Printf("%v\n", counts)
 }
 
-func TestGen(t *testing.T) {
-	fmt.Printf("NextUniform => %f\n", NextUniform());
-	fmt.Printf("NextExp => %f\n", NextExp(1.5));
-	fmt.Printf("NextGamma => %f\n", NextGamma(.3, 1));
-	fmt.Printf("NextNormal => %f\n", NextNormal(0, 1));
-	fmt.Printf("NextRange => %d\n", NextRange(10));
-	fmt.Printf("NextChoice => %d\n", NextChoice([]float64{.3, .3, .4}));
+func TestLnGamma(t *testing.T) {
+	acc := 0.0000001
+	check := func(x, y float64) bool {
+		if false {
+			return x == y
+		}
+		return math.Fabs(x-y) < acc
+	}
+	for i := 0; i < 100; i++ {
+		x := NextGamma(10, 10)
+		g1 := LnΓ(x)
+		g2, _ := math.Lgamma(x)
+		if !check(g1, g2) {
+			t.Error(fmt.Sprintf("For %v: %v vs %v", x, g1, g2))
+		}
+	}
+	var start int64
+	Seed(10)
+	start = time.Nanoseconds()
+	for i := 0; i < 1e6; i++ {
+		x := NextGamma(10, 10)
+		math.Lgamma(x)
+	}
+	duration2 := float64(time.Nanoseconds()-start) / 1e9
+	Seed(10)
+	start = time.Nanoseconds()
+	for i := 0; i < 1e6; i++ {
+		x := NextGamma(10, 10)
+		LnΓ(x)
+	}
+	duration1 := float64(time.Nanoseconds()-start) / 1e9
+	fmt.Printf("Mine was %f\nTheirs was %f\n", duration1, duration2)
+}
+
+func XTestGen(t *testing.T) {
+	fmt.Printf("NextUniform => %f\n", NextUniform())
+	fmt.Printf("NextExp => %f\n", NextExp(1.5))
+	fmt.Printf("NextGamma => %f\n", NextGamma(.3, 1))
+	fmt.Printf("NextNormal => %f\n", NextNormal(0, 1))
+	fmt.Printf("NextRange => %d\n", NextRange(10))
+	fmt.Printf("NextChoice => %d\n", NextChoice([]float64{.3, .3, .4}))
 	fmt.Printf("NextMultinomial => %v\n",
-		NextMultinomial([]float64{.3, .3, .4}, 100));
-	fmt.Printf("NextDirichlet => %v\n", NextDirichlet([]float64{.3, .3, .4}));
-	fmt.Printf("NextBernoulli => %d\n", NextBernoulli(.5));
-	fmt.Printf("NextGeometric => %d\n", NextGeometric(.5));
-	fmt.Printf("NextBinomial => %d\n", NextBinomial(.5, 10));
-	fmt.Printf("NextPoisson => %d\n", NextPoisson(1.5));
-	fmt.Printf("NextXsquare => %f\n", NextXsquare(3));
-	fmt.Printf("NextNegativeBinomial => %d\n", NextNegativeBinomial(.5, 10));
-	fmt.Printf("NextStudentsT => %f\n", NextStudentsT(7));
-	fmt.Printf("NextF => %f\n", NextF(7, 3));
+		NextMultinomial([]float64{.3, .3, .4}, 100))
+	fmt.Printf("NextDirichlet => %v\n", NextDirichlet([]float64{.3, .3, .4}))
+	fmt.Printf("NextBernoulli => %d\n", NextBernoulli(.5))
+	fmt.Printf("NextGeometric => %d\n", NextGeometric(.5))
+	fmt.Printf("NextBinomial => %d\n", NextBinomial(.5, 10))
+	fmt.Printf("NextPoisson => %d\n", NextPoisson(1.5))
+	fmt.Printf("NextXsquare => %f\n", NextXsquare(3))
+	fmt.Printf("NextNegativeBinomial => %d\n", NextNegativeBinomial(.5, 10))
+	fmt.Printf("NextStudentsT => %f\n", NextStudentsT(7))
+	fmt.Printf("NextF => %f\n", NextF(7, 3))
 	fmt.Printf("NextWishart => %v\n",
-		NextWishart(100, [][]float64{[]float64{1, 0}, []float64{0, 1}}));
+		NextWishart(100, [][]float64{[]float64{1, 0}, []float64{0, 1}}))
 	fmt.Printf("NextInverseWishart => %v\n",
-		NextInverseWishart(100, [][]float64{[]float64{1, 0}, []float64{0, 1}}));
+		NextInverseWishart(100, [][]float64{[]float64{1, 0}, []float64{0, 1}}))
 }
